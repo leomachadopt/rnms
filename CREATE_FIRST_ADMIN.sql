763443338a6de32aeaad891b22007e3285d9d039
leomachadopt <leomachadopt@gmail.com>
2026-02-02 18:03:40 +0000

    fix: Atualizar hash bcrypt no script de criação do primeiro admin

--- CREATE_FIRST_ADMIN.sql
+++ CREATE_FIRST_ADMIN.sql
@@ -1,13 +1,10 @@
 -- Script para criar o primeiro usuário super_admin
--- Execute este SQL no console do Neon Database
-
 -- A senha é: Admin123!
--- Hash bcrypt gerado para 'Admin123!': $2a$10$YourHashHere
 
 INSERT INTO users (email, password, name, role, active)
 VALUES (
   'leomachadopt@gmail.com',
-  '$2a$10$rOV5PZQmZ8qKNzC9uXhxE.8QEKHNxX5YJj8uxKFGXBZQvGWm4nzKm', -- Hash de 'Admin123!'
+  '$2b$10$GhrNTT8nHB1KXcR6TSOo8.PYya3VQOky2nV908dCXI7Q9cPmvZMry', -- Hash de 'Admin123!'
   'Leonardo Machado',
   'super_admin',
   1
